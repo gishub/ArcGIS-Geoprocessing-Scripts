3b6eb6ef134d06539acbd5516ce3c757848442f1
Andrew Cottam <a.cottam@gmail.com>
2011-07-15 17:22:01 +0200

    Updated to reset the extent after each species so it can copy the features

--- src/RasteriseIUCNSpecies.py
+++ src/RasteriseIUCNSpecies.py
@@ -13,28 +13,23 @@ SPECIES_TABLE = "SpeciesData"
 EXTENT_FC = "in_memory\\RangesFC"
 
 #INPUT PARAMETERS
-speciesFCPath = arcpy.GetParameterAsText(0)
+speciesFL = arcpy.GetParameterAsText(0)
 rasterWorkspace = arcpy.GetParameter(1)
-scratchFC = str(rasterWorkspace) + "\\tmp"
+scratchFC = r"E:\cottaan\My Documents\ArcGIS\tmp.shp"
 
 #ENVIRONMENT VARIABLES
 arcpy.env.overwriteOutput = True
 
 #ADD THE PRIORITY FIELD IF IT IS NOT ALREADY PRESENT
-if (len(arcpy.ListFields(speciesFCPath, PRIORITY_FIELDNAME))==0):
+if (len(arcpy.ListFields(speciesFL, PRIORITY_FIELDNAME))==0):
     arcpy.AddMessage("Adding priority field to species feature class")
-    arcpy.AddField_management(speciesFCPath, PRIORITY_FIELDNAME,"LONG")
+    arcpy.AddField_management(speciesFL, PRIORITY_FIELDNAME,"LONG")
     arcpy.AddMessage("Populating priority field")
     arcpy.CalculateField_management(speciesFCPath, PRIORITY_FIELDNAME,1)
     
-#CREATE AN IN-MEMORY FC OF THE SPECIES
-arcpy.AddMessage("Creating Feature Layer for all species")
-arcpy.MakeFeatureLayer_management(speciesFCPath, ALL_SPECIES_FC)
-
 #CREATE A TABLE OF UNIQUE SPECIES TO ITERATE THROUGH
 arcpy.AddMessage("Creating unique species table")
-arcpy.Frequency_analysis(ALL_SPECIES_FC, SPECIES_TABLE, "ID_NO")
-#arcpy.MakeTableView_management(r"C:\Users\AndrewCottam\Documents\ArcGIS\Default.gdb\SpeciesTable",SPECIES_TABLE)
+arcpy.Frequency_analysis(speciesFL, SPECIES_TABLE, "ID_NO")
 
 #ITERATE THROUGH THE SPECIES TO OUTPUT THE RASTER FOR EACH ONE
 arcpy.AddMessage("Iterating through species")
@@ -44,10 +39,10 @@ for species in AllSpecies:
     if (id!=" "):# for some reason a NULL is a space in the FREQUENCY table
         arcpy.AddMessage("Species ID:" + id)
         arcpy.AddMessage("Selecting features")
-#        arcpy.MakeFeatureLayer_management(ALL_SPECIES_FC, SPECIES_FC, "ID_NO='" + id + "'")        
-        arcpy.SelectLayerByAttribute_management(ALL_SPECIES_FC, "NEW_SELECTION", "ID_NO='" + id + "'")
+        arcpy.SelectLayerByAttribute_management(speciesFL, "NEW_SELECTION", "ID_NO='" + id + "'")
         arcpy.AddMessage("Copying features")
-        arcpy.CopyFeatures_management(ALL_SPECIES_FC, scratchFC)
+        arcpy.CopyFeatures_management(speciesFL, scratchFC)
+        arcpy.AddMessage("Counting features")
         count = arcpy.GetCount_management(scratchFC)
         arcpy.AddMessage("Getting extent")
         if (count==1):
@@ -70,6 +65,8 @@ for species in AllSpecies:
         arcpy.AddMessage("maxx:" + str(maxx))
         arcpy.AddMessage("miny:" + str(miny))
         arcpy.AddMessage("maxy:" + str(maxy))
-        arcpy.AddMessage("Creating raster" + str(rasterWorkspace) + "\\ID" + id)
+        arcpy.AddMessage("Creating raster: " + str(rasterWorkspace) + "\\ID" + id)
         arcpy.env.extent = arcpy.Extent(minx, miny, maxx, maxy)
         arcpy.PolygonToRaster_conversion(scratchFC, "ID_NO", str(rasterWorkspace) + "\\ID" + id, "MAXIMUM_AREA", PRIORITY_FIELDNAME, 1000)
+        arcpy.env.extent = None #Reset the extent
+        arcpy.Delete_management(scratchFC)
