59fb9aa4e053d18b95e4006cddfef44b8f82618a
Andrew Cottam <a.cottam@gmail.com>
2011-07-15 14:57:47 +0200

    Rasterise first version working

--- src/RasteriseIUCNSpecies.py
+++ src/RasteriseIUCNSpecies.py
@@ -1,51 +1,70 @@
-'''
-Created on Jul 14, 2011
-
-@author: AndrewCottam
-'''
 import arcpy
+
 def getSnapMetres(coord,up):
     if (up==True):
         return (((int(coord/1000))+1)*1000)
     else:
         return (((int(coord/1000))-1)*1000)
-#TODO: Add in the code to add a Priority field and populate it with 1s
-print "Creating Feature Layer for all species"
-arcpy.MakeFeatureLayer_management(r"D:\GIS Data\IUCN\OverlayAnalysis.gdb\Species", "Ranges")
-print "Creating unique species table"
-#arcpy.Frequency_analysis("Ranges", "SpeciesTable", "ID_NO")
-arcpy.MakeTableView_management(r"C:\Users\AndrewCottam\Documents\ArcGIS\Default.gdb\SpeciesTable","SpeciesData")
-AllSpecies = arcpy.SearchCursor("SpeciesData")
-print ("Iterating through species")
+
+#CONSTANT DECLARATIONS
+PRIORITY_FIELDNAME = "Priority"
+ALL_SPECIES_FC = "Ranges"
+SPECIES_TABLE = "SpeciesData"
+SPECIES_FC = "SpeciesLayer"
+EXTENT_FC = "in_memory\\RangesFC"
+
+#INPUT PARAMETERS
+speciesFCPath = arcpy.GetParameterAsText(0)
+rasterWorkspace = arcpy.GetParameter(1)
+
+#ADD THE PRIORITY FIELD IF IT IS NOT ALREADY PRESENT
+if (len(arcpy.ListFields(speciesFCPath, PRIORITY_FIELDNAME))==0):
+    arcpy.AddMessage("Adding priority field to species feature class")
+    arcpy.AddField_management(speciesFCPath, PRIORITY_FIELDNAME,"LONG")
+    arcpy.AddMessage("Populating priority field")
+    arcpy.CalculateField_management(speciesFCPath, PRIORITY_FIELDNAME,1)
+    
+#CREATE AN IN-MEMORY FC OF THE SPECIES
+arcpy.AddMessage("Creating Feature Layer for all species")
+arcpy.MakeFeatureLayer_management(speciesFCPath, ALL_SPECIES_FC)
+
+#CREATE A TABLE OF UNIQUE SPECIES TO ITERATE THROUGH
+arcpy.AddMessage("Creating unique species table")
+arcpy.Frequency_analysis(ALL_SPECIES_FC, SPECIES_TABLE, "ID_NO")
+#arcpy.MakeTableView_management(r"C:\Users\AndrewCottam\Documents\ArcGIS\Default.gdb\SpeciesTable",SPECIES_TABLE)
+
+#ITERATE THROUGH THE SPECIES TO OUTPUT THE RASTER FOR EACH ONE
+arcpy.AddMessage("Iterating through species")
+AllSpecies = arcpy.SearchCursor(SPECIES_TABLE)
 for species in AllSpecies:
     id = species.ID_NO    
     if (id!=" "):# for some reason a NULL is a space in the FREQUENCY table
-        print "Species ID:" + id
         arcpy.AddMessage("Species ID:" + id)
-        arcpy.MakeFeatureLayer_management("Ranges", "SpeciesLayer", "ID_NO='" + id + "'")
-        count = arcpy.GetCount_management("SpeciesLayer")
+        arcpy.AddMessage("Selecting features")
+        arcpy.MakeFeatureLayer_management(ALL_SPECIES_FC, SPECIES_FC, "ID_NO='" + id + "'")
+        count = arcpy.GetCount_management(SPECIES_FC)
+        arcpy.AddMessage("Getting extent")
         if (count==1):
-            features = arcpy.SearchCursor("SpeciesLayer","ID_NO='" + id + "'")
+            features = arcpy.SearchCursor(SPECIES_FC, "ID_NO='" + id + "'")
             for feature in features:
                 geometry = feature.Shape
                 extent = geometry.Extent
             del features
         else:    
-            arcpy.MinimumBoundingGeometry_management("SpeciesLayer", "in_memory\\RangesFC","ENVELOPE","ALL")
-            dsc = arcpy.Describe("in_memory\RangesFC")
+            arcpy.MinimumBoundingGeometry_management(SPECIES_FC, EXTENT_FC, "ENVELOPE", "ALL")
+            dsc = arcpy.Describe(EXTENT_FC)
             extent = dsc.Extent    
-            arcpy.Delete_management("in_memory\\RangesFC")
-            minx = getSnapMetres(extent.XMin,False)
-            maxx = getSnapMetres(extent.XMax,True)
-            miny = getSnapMetres(extent.YMin,False)
-            maxy = getSnapMetres(extent.YMax,True)
-            print "minx:" + str(minx)
-            print "maxx:" + str(maxx)
-            print "miny:" + str(miny)
-            print "maxy:" + str(maxy)
-            arcpy.env.extent = arcpy.Extent(minx, miny, maxx, maxy)
-            arcpy.PolygonToRaster_conversion("SpeciesLayer", "ID_NO", r"D:\GIS Data\Andrew\MapperDemo.gdb\ID" + id, "MAXIMUM_AREA", "Priority", 1000)
-            arcpy.Delete_management("SpeciesLayer")
-            
-print "End"
-        
+            arcpy.Delete_management(EXTENT_FC)
+        arcpy.AddMessage("Calculating raster extent")
+        minx = getSnapMetres(extent.XMin, False)
+        maxx = getSnapMetres(extent.XMax, True)
+        miny = getSnapMetres(extent.YMin, False)
+        maxy = getSnapMetres(extent.YMax, True)
+        arcpy.AddMessage("minx:" + str(minx))
+        arcpy.AddMessage("maxx:" + str(maxx))
+        arcpy.AddMessage("miny:" + str(miny))
+        arcpy.AddMessage("maxy:" + str(maxy))
+        arcpy.AddMessage("Creating raster" + str(rasterWorkspace) + "\\ID" + id)
+        arcpy.env.extent = arcpy.Extent(minx, miny, maxx, maxy)
+        arcpy.PolygonToRaster_conversion(SPECIES_FC, "ID_NO", str(rasterWorkspace) + "\\ID" + id, "MAXIMUM_AREA", PRIORITY_FIELDNAME, 1000)
+        arcpy.Delete_management(SPECIES_FC)
