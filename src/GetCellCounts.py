f3d7c06f2dfa01c4d19bf122c9fe33231b160520
AndrewCottam <a.cottam@gmail.com>
2011-09-13 12:25:18 +0100

    minor edit

--- src/GetCellCounts.py
+++ src/GetCellCounts.py
@@ -4,4 +4,4 @@ for file in arcpy.ListFiles("*.npy"):
     fileinfo=os.stat(arcpy.env.workspace + os.sep + file)
     if fileinfo.st_size>0:
         cells=numpy.load(arcpy.env.workspace + os.sep + file)
-        arcpy.AddMessage(file + "\t" + str(len(cells[0])))
+        arcpy.AddMessage(file + "\t" + str(len(cells[0]))) 
