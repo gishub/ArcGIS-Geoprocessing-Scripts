93361b7c78376f5a5f6440f0399eb7797bbfe25e
Andrew Cottam <a.cottam@gmail.com>
2011-09-22 13:14:15 +0200

    small edit

--- src/GetCellCounts.py
+++ src/GetCellCounts.py
@@ -5,3 +5,4 @@ for file in arcpy.ListFiles("*.npy"):
     if fileinfo.st_size>0:
         cells=numpy.load(arcpy.env.workspace + os.sep + file)
         arcpy.AddMessage(file + "\t" + str(len(cells)/3)) #the data are saved as x array, y array and values array
+        del cells
