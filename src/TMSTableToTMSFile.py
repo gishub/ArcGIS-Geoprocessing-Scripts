bea6d2da258a786badfe4f539e85086f3f72c0d0
Andrew Cottam <a.cottam@gmail.com>
2011-10-14 14:24:33 +0200

    Array concatenation now works

--- src/TMSTableToTMSFile.py
+++ src/TMSTableToTMSFile.py
@@ -1,11 +1,10 @@
-import numpy
-import arcpy
+import numpy,arcpy,cPickle
 from scipy.sparse import *
 from scipy import *
 table=arcpy.GetParameterAsText(0)
 outputfile=arcpy.GetParameterAsText(1)
 count = int(str(arcpy.GetCount_management(table)))
-rows = arcpy.SearchCursor(table) 
+rows = arcpy.SearchCursor(table)
 xarr=numpy.zeros([count],int) #initialise an array to hold the data for the x coordinates
 yarr=numpy.zeros([count],int) #initialise an array to hold the data for the y coordinates
 zarr=numpy.zeros([count],int) #initialise an array to hold the data for the species richness
@@ -15,7 +14,7 @@ for row in rows: #iterate through the rows
     yarr.put(i,row.ty)
     zarr.put(i,row.COUNT_speciesID)    
     i = i + 1
-outputArr=numpy.concatenate(xarr,yarr,zarr) #join the 3 arrays together 
+outputArr=numpy.concatenate((xarr,yarr,zarr)) #join the 3 arrays together 
 f = open(outputfile,'wb') #write the ndarray out to file
 cPickle.dump(outputArr, f, protocol=2)
 f.close() 
