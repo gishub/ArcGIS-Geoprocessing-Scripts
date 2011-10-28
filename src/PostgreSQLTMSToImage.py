c7f42414a41937997f6e8ba287531efeaaa8c4f8
Andrew Cottam <a.cottam@gmail.com>
2011-10-28 10:24:39 +0200

    Added in benchmarking code

--- src/PostgreSQLTMSToImage.py
+++ src/PostgreSQLTMSToImage.py
@@ -3,14 +3,21 @@ import numpy
 import Image
 import arcpy
 from scipy.sparse import *
+import time
+t0=time.time()
 CELLSIZE = 1222.9924525618553 #cell size at zoom 15
 OFFSET = 20037508.3428        #offset for the web mercator projection
 speciesID= arcpy.GetParameterAsText(0)
 conn = psycopg2.connect(host="durga.jrc.org", database="dbdopa", user="usrdopa", password="W25e12b")
 cur = conn.cursor()
-cur.execute("SELECT tx,ty,z FROM public.pilotspeciesdata WHERE speciesid='" + speciesID + "'")
+cur.execute("SELECT tx,ty,z FROM public.pilotspeciesdata WHERE speciesid in (" + speciesID + ")")
+arcpy.AddMessage("Number records: " + str(len(rows)))
 rows=cur.fetchall()
+t1=time.time()
+arcpy.AddMessage("Records returned: " + str(t1-t0) + "ms")
 data3d=numpy.transpose(numpy.array(rows))
+t2=time.time()
+arcpy.AddMessage("Transposed: " + str(t2-t1) + "ms")
 minx=min(data3d[0]) #get the minx value
 maxx=max(data3d[0]) #get the maxx value
 miny=min(data3d[1]) #get the miny value
@@ -21,9 +28,13 @@ data3d[0].__isub__(minx) #change the x values to be zero based by subtracting th
 data3d[1].__imul__(-1)   #do the same with the y values using a different calculation
 data3d[1].__iadd__(maxy)
 pixels=coo_matrix((data3d[2],(data3d[1],data3d[0])), shape=(height,width)).todense() #convert the sparse array into a dense matrix, i.e. by adding in all of the zero values
+t3=time.time()
+arcpy.AddMessage("Converted to dense matrix: " + str(t3-t2) + "ms")
 image = Image.fromarray(pixels) #create the output tif from the pixel values
-imagefile=r"E:\cottaan\My Documents\ArcGIS\ID" + speciesID + ".tif"
+imagefile=r"E:\cottaan\My Documents\ArcGIS\species.tif"
 image.save(imagefile) #save the image to a file
+t4=time.time()
+arcpy.AddMessage("Created image: " + str(t4-t3) + "ms")
 f = open(imagefile[:-3] + "tfw",'w') #open the tfw file to write the georeferencing coordinates in
 f.write(str(CELLSIZE) + "\n0.0000000000\n0.0000000000\n-" + str(CELLSIZE) + "\n") #you need to set the y cell size as a minus number otherwise the image is upside down
 topleftx=(minx*CELLSIZE)- OFFSET + (CELLSIZE/2) #get the top left x coordinate
@@ -31,3 +42,5 @@ toplefty=(maxy*CELLSIZE)- OFFSET + (CELLSIZE/2) #get the top left y coordinate
 f.write(str(topleftx) + "\n")  #top left x coordinate
 f.write(str(toplefty) + "\n")  #top left y coordinate
 f.close() #close the file
+t5=time.time()
+arcpy.AddMessage("TOTAL TIME: " + str(t5-t0) + "ms")
