8c36a3f29cfbf0bf813c3ea687a1ed42947aeea6
Andrew Cottam <a.cottam@gmail.com>
2012-01-24 08:44:14 +0100

    Added logic to be able to restart the intersect after a crash

--- src/IntersectWDPAAndSpecies2011.py
+++ src/IntersectWDPAAndSpecies2011.py
@@ -1,23 +1,29 @@
 import arcpy
 arcpy.AddMessage("Creating intersections table")
-outputTable = arcpy.CreateTable_management(r"E:\cottaan\My Documents\ArcGIS\Default.gdb","Africa_Species_PA_Intersections")
-arcpy.AddField_management(outputTable,"ID_NO","TEXT")
-arcpy.AddField_management(outputTable,"wdpaid","LONG")
+outputTable = arcpy.CreateTable_management(r"E:\cottaan\My Documents\ArcGIS\Default.gdb", "Africa_Species_PA_Intersections")
+arcpy.AddField_management(outputTable, "ID_NO", "TEXT")
+arcpy.AddField_management(outputTable, "wdpaid", "LONG")
 insertCursor = arcpy.InsertCursor(r"E:\cottaan\My Documents\ArcGIS\Default.gdb\Africa_Species_PA_Intersections")
 rows = arcpy.SearchCursor("African_Species2011")
 rowCount = arcpy.GetCount_management("African_Species2011")
-i=0
+i = 0
+start = False
 for row in rows:
-    i=i+1
-    arcpy.SelectLayerByAttribute_management("African_Species2011","NEW_SELECTION","ID_NO='" + row.ID_NO + "'")
-    arcpy.SelectLayerByLocation_management("African_Protected_Areas","INTERSECT","African_Species2011")
-    paCursor = arcpy.SearchCursor("African_Protected_Areas")
-    for pa in paCursor:
-        newRow = insertCursor.newRow()
-        newRow.ID_NO = row.ID_NO
-        newRow.wdpaid = pa.wdpaid
-        insertCursor.insertRow(newRow)
-    del newRow
-    arcpy.AddMessage(row.ID_NO + "\t" + row.BINOMIAL + " (" + str(i) + " out of " + str(rowCount) + ") (" + str(datetime.datetime.now()) + ")")
+    i = i + 1
+    if (row.ID_NO == '4310'): # can be used to start the code at a particular ID_NO again if it crashes 
+        start = True
+    if (start == True):
+        arcpy.SelectLayerByAttribute_management("African_Species2011", "NEW_SELECTION", "ID_NO='" + row.ID_NO + "'")
+        arcpy.SelectLayerByLocation_management("African_Protected_Areas", "INTERSECT", "African_Species2011")
+        paCursor = arcpy.SearchCursor("African_Protected_Areas")
+        for pa in paCursor:
+            newRow = insertCursor.newRow()
+            newRow.ID_NO = row.ID_NO
+            newRow.wdpaid = pa.wdpaid
+            insertCursor.insertRow(newRow)
+        del newRow
+        arcpy.AddMessage(row.ID_NO + "\t" + row.BINOMIAL + " (" + str(i) + " out of " + str(rowCount) + ") (" + str(datetime.datetime.now()) + ")")
+    else:
+        arcpy.AddMessage("Skipping: " + row.ID_NO + "\t" + row.BINOMIAL + " (" + str(i) + " out of " + str(rowCount) + ") (" + str(datetime.datetime.now()) + ")")
     
     
