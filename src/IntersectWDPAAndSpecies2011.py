dbe278dfcfd3dec34371a2e24489c2f7f23071a5
Andrew Cottam <a.cottam@gmail.com>
2012-02-09 16:04:41 +0100

    Parameterised the code

--- src/IntersectWDPAAndSpecies2011.py
+++ src/IntersectWDPAAndSpecies2011.py
@@ -1,28 +1,32 @@
 import arcpy
+workspace=arcpy.GetParameterAsText(0)
+tablename=arcpy.GetParameterAsText(1)
+speciesLayer=arcpy.GetParameterAsText(2)
+wdpaLayer=arcpy.GetParameterAsText(3)
+outputTable = arcpy.CreateTable_management(workspace, tablename)
 arcpy.AddMessage("Creating intersections table")
-outputTable = arcpy.CreateTable_management(r"E:\cottaan\My Documents\ArcGIS\Default.gdb", "Africa_Species_PA_Intersections")
 arcpy.AddField_management(outputTable, "ID_NO", "TEXT")
 arcpy.AddField_management(outputTable, "wdpaid", "LONG")
-insertCursor = arcpy.InsertCursor(r"E:\cottaan\My Documents\ArcGIS\Default.gdb\Africa_Species_PA_Intersections")
-rows = arcpy.SearchCursor("African_Species2011")
-rowCount = arcpy.GetCount_management("African_Species2011")
+insertCursor = arcpy.InsertCursor(outputTable)
+rows = arcpy.SearchCursor(speciesLayer)
+rowCount = arcpy.GetCount_management(speciesLayer)
 i = 0
-start = False
+start = True
 for row in rows:
     i = i + 1
-    if (row.ID_NO == '4310'): # can be used to start the code at a particular ID_NO again if it crashes 
-        start = True
+#    if (row.ID_NO == '4310'): # can be used to start the code at a particular ID_NO again if it crashes 
+#        start = True
     if (start == True):
-        arcpy.SelectLayerByAttribute_management("African_Species2011", "NEW_SELECTION", "ID_NO='" + row.ID_NO + "'")
-        arcpy.SelectLayerByLocation_management("African_Protected_Areas", "INTERSECT", "African_Species2011")
-        paCursor = arcpy.SearchCursor("African_Protected_Areas")
+        arcpy.AddMessage("Processing: " + row.ID_NO + "\t" + row.BINOMIAL + " (" + str(i) + " out of " + str(rowCount) + ") (" + str(datetime.datetime.now()) + ")")
+        arcpy.SelectLayerByAttribute_management(speciesLayer, "NEW_SELECTION", "ID_NO='" + row.ID_NO + "'")
+        arcpy.SelectLayerByLocation_management(wdpaLayer, "INTERSECT", speciesLayer)
+        paCursor = arcpy.SearchCursor(wdpaLayer)
         for pa in paCursor:
             newRow = insertCursor.newRow()
             newRow.ID_NO = row.ID_NO
             newRow.wdpaid = pa.wdpaid
             insertCursor.insertRow(newRow)
         del newRow
-        arcpy.AddMessage(row.ID_NO + "\t" + row.BINOMIAL + " (" + str(i) + " out of " + str(rowCount) + ") (" + str(datetime.datetime.now()) + ")")
     else:
         arcpy.AddMessage("Skipping: " + row.ID_NO + "\t" + row.BINOMIAL + " (" + str(i) + " out of " + str(rowCount) + ") (" + str(datetime.datetime.now()) + ")")
     
