acf60790fa027673ed51b6f80dd6453f26b2364d
Andrew Cottam <a.cottam@gmail.com>
2014-07-09 13:49:59 +0200

    Added a script to download all of the zipped SWDB data

---
+++ src/downloadSWDB.py
@@ -0,0 +1,18 @@
+import urllib, zipfile, arcpy, re, os
+HTTP_ADDRESS = "http://dds.cr.usgs.gov/srtm/version2_1/SWBD/"
+for folder in ['SWBDwest', 'SWBDeast']:
+    arcpy.AddMessage("Getting file information for " + folder)
+    html = urllib.urlopen(HTTP_ADDRESS + folder + "/")
+    text = html.read()
+    html.close()
+    filenames = [s[1:] for s in re.findall("[^ ][ew][0-9]*[ns][0-9]*[afiens].zip", text)]
+    arcpy.AddMessage(str(len(filenames)) + " files found for " + folder)
+    counter = 1
+    for filename in filenames:
+        fullfilename = HTTP_ADDRESS + folder + "/" + filename
+        arcpy.AddMessage("Downloading " + fullfilename + " (" + str(counter) + "/" + str(len(filenames)) + ")")
+        f, h = urllib.urlretrieve(fullfilename)
+        zip = zipfile.ZipFile(f)
+        zip.extractall("E:/cottaan/My Documents/ArcGIS/SWDB")
+        arcpy.AddMessage("Extracting " + os.path.basename(fullfilename) + "\n")
+        counter = counter + 1
