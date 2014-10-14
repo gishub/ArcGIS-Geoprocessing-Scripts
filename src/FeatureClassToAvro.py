be20226cacef37897a77c1ccace5b767f5bd9a8b
Andrew Cottam <a.cottam@gmail.com>
2014-10-14 10:10:21 +0200

    avro schema written on-the-fly from the input featureclass

--- src/FeatureClassToAvro.py
+++ src/FeatureClassToAvro.py
@@ -1,29 +1,44 @@
-import sys, arcpy
+import sys, arcpy, json
 sys.path.append(r"C:\Anaconda\Lib\site-packages")
 import avro
 import avro.schema
 from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
+from StringIO import StringIO
 
 # get the feature class
-# fc = r'E:\cottaan\My Documents\ArcGIS\tmp\Test_FileGDB.gdb\WGS84\Species'
-fc = r'E:\cottaan\My Documents\ArcGIS\iucn_rl_species_2014_2.gdb\iucn_rl_species_2014_2_no_sens_mollweide'
-
-# get the schema
-schema = avro.schema.parse(open("species_2014.avsc").read())
-
-# open a writer to write the data
-writer = DataFileWriter(open("species.avro", "w"), DatumWriter(), schema)
+fc = r'E:\cottaan\My Documents\ArcGIS\iucn_rl_species_2014_2.gdb\wdpa_aug14_100km2_moll'
+desc = arcpy.Describe(fc)
 
 # get the field list for the feature class
 fields = arcpy.ListFields(fc)
+fieldsArray = []
+for field in fields:
+    if field.type == "OID":
+        fieldType = 'long'
+    elif field.type == "Geometry":
+        fieldType = 'bytes'
+    else:
+        fieldType = field.type.lower()
+    if field.isNullable:
+        fieldsArray.append({"name": field.name, "type": [fieldType, "null"]})
+    else:
+        fieldsArray.append({"name": field.name, "type": fieldType})
 
+# create the schema from the fields
+schemajson = {"type": "record", "name": desc.name, "fields": fieldsArray }
+schema = avro.schema.parse(json.dumps(schemajson, None))
+print "Schema created"
+
+# open a writer to write the data
+writer = DataFileWriter(open("E:/cottaan/My Documents/" + desc.name + ".avro", "w"), DatumWriter(), schema)
+ 
 # get a cursor to the records
 cursor = arcpy.SearchCursor(fc)
-
+ 
 # get the count
 total = str(arcpy.GetCount_management(fc))
-
+ 
 # iterate through the records and write the dtaa
 row = cursor.next()
 count = 1
@@ -32,18 +47,13 @@ while row:
     data = {}
     for field in fields:
         if row.getValue(field.name) != None:
-            if field.name == "shape":
-                data["shape"] = str(row.getValue('shape').WKB)  
+            if field.name.lower() == "shape":
+                data[field.name] = str(row.getValue(field.name).WKB)  
             else:
-                if field.name.upper() in ["OBJECTID","ID_NO","BINOMIAL","PRESENCE","ORIGIN","COMPILER","YEAR","CITATION","SOURCE","DIST_COMM","ISLAND","SUBSPECIES","SUBPOP","TAX_COMMEN","DATA_SENS","SENS_COMM","LEGEND","SEASONAL","OWNER","SPECIESID","ECOSYSTEM"]:
-                    data[field.name] = row.getValue(field.name)
+                data[field.name] = row.getValue(field.name)
     writer.append(data)
     row = cursor.next()
     count = count + 1
-    break 
+    break
 writer.close()
-# 
-# reader = DataFileReader(open("users.avro", "r"), DatumReader())
-# for user in reader:
-#     print user
-# reader.close()
+print "Data written to " + desc.name + ".avro"
